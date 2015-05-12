d48e091bbdc3d677ebf1cf6c3a934d3f48a4cce6
grandmasteryang <xuyang2016@u.northwestern.edu>
2015-05-12 14:27:46 -0500

    added html camera, overlay not working and need to switch to rear camer

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -1,8 +1,3 @@
-angular.module('example', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
-  'supersonic'
-]);
-
 angular
   .module('example')
   .controller('GettingStartedController', function($scope, supersonic) {
@@ -53,18 +48,4 @@ function convertCanvasToImage(canvas) {
 }
    
 
-  });
-
-angular
-  .module('example')
-  .controller('LearnMoreController', function($scope, supersonic) {
-
-    $scope.navbarTitle = "Learn More";
-
-  });
-
-angular
-  .module('example')
-  .controller('SettingsController', function($scope, supersonic) {
-    $scope.navbarTitle = "Settings";
   });
