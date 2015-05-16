57296924fc65da1897ce2fa1b1d09b4a48ca360f
Yanghg <yanghong2014@u.northwestern.edu>
2015-05-16 14:23:05 -0500

    change buttons

---
+++ dist/app/example.js
@@ -0,0 +1,70 @@
+angular.module('example', [
+  // Declare here all AngularJS dependencies that are shared by the example module.
+  'supersonic'
+]);
+
+angular
+  .module('example')
+  .controller('GettingStartedController', function($scope, supersonic) {
+  	// Put event listeners into place
+window.addEventListener("DOMContentLoaded", function() {
+	// Grab elements, create settings, etc.
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		video = document.getElementById("video"),
+		videoObj = { "video": true },
+		errBack = function(error) {
+			console.log("Video capture error: ", error.code); 
+		};
+
+	// Trigger photo take
+	document.getElementById("snap").addEventListener("click", function() {
+		context.drawImage(video, 0, 0, 320, 320);
+		//return convertCanvasToImage(canvas);
+	});
+
+	// Put video listeners into place
+	if(navigator.getUserMedia) { // Standard
+		navigator.getUserMedia(videoObj, function(stream) {
+			video.src = stream;
+			video.play();
+		}, errBack);
+	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+		navigator.webkitGetUserMedia(videoObj, function(stream){
+			video.src = window.webkitURL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+	else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+		navigator.mozGetUserMedia(videoObj, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+}, false);
+
+
+
+// Converts canvas to an image
+function convertCanvasToImage(canvas) {
+	var image = new Image();
+	image.src = canvas.toDataURL("image/png");
+	return image;
+}
+   
+
+  });
+
+angular
+  .module('example')
+  .controller('LearnMoreController', function($scope, supersonic) {
+
+    $scope.navbarTitle = "Learn More";
+
+  });
+
+angular
+  .module('example')
+  .controller('SettingsController', function($scope, supersonic) {
+    $scope.navbarTitle = "Settings";
+  });
