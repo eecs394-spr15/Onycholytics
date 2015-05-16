2163ec98127b75308d7b0ffe0ea295021ba10aaf
Yanghg <yanghong2014@u.northwestern.edu>
2015-05-16 13:48:18 -0500

    standardize

--- dist/app/camera.js
+++ dist/app/camera.js
@@ -8,14 +8,17 @@ angular
   .controller("ImageController", function ($scope, Progresstable, supersonic) {
 
     var draggable = document.getElementById('draggable');
+    var template = document.getElementById('template');
     draggable.addEventListener('touchmove', function(event) {
         var touch = event.targetTouches[0];
- 
+        var top = 310;
     // Place element where the finger is
-        draggable.style.left = touch.pageX-25 + 'px';
-        draggable.style.top = touch.pageY-25 + 'px';
-        draggable.width = currentWidth*1.2; 
-        draggable.height = currentHeight*1.2; 
+        draggable.style.left = touch.pageX-100 + 'px';
+        if (touch.pageY+20 > top)
+            draggable.style.top = top + 'px';
+        else
+            draggable.style.top = touch.pageY+20 + 'px';
+        
         event.preventDefault();
     }, false);
     $scope.zoomIn = function() {
@@ -37,6 +40,7 @@ angular
         update(); 
     } 
    $scope.initial=function(){ 
+        //template.style.left = 200;
         currentWidth = document.myImage.width; 
         currentHeight = document.myImage.height; 
         originalWidth = currentWidth; 
